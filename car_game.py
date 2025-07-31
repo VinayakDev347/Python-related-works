8ab0241c7d42ad9a3c93172f2e3e97a8b24bdb03
vinayak-dev <vinayakdev347@gmail.com>
2025-07-31 07:25:56 +0530

    Updated car_game.py with new features/fixes

--- car_game.py
+++ car_game.py
@@ -1,15 +1,26 @@
 command = ""
-while command != "quit":
+started = False
+while True:
     command = input("> ").lower()
     if command == "start":
-        print("Car Started...")
+        if started:
+            print("Car is already started!")
+        else:
+            started = True
+            print("Car Started...")
     elif command == "stop":
-        print("Car Stopped...")
+        if not started:
+            print("Car is already stopped!")
+        else:
+            started = False 
+            print("Car Stopped...")
     elif command == "help":
         print("""
             start - To Start The Car !
             stop - To Stop The Car !
             quit - To Quit !
               """)
+    elif command == quit:
+        break
     else:
         print("No Response!!!")
