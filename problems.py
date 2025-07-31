b591b0034499b4f0455ba6d92e5c19c5498f64fe
vinayak-dev <vinayakdev347@gmail.com>
2025-07-31 19:23:19 +0530

    Added removing duplicate number from the list Problem

--- problems.py
+++ problems.py
@@ -1,9 +1,23 @@
-#write a program to find the largest number from the list
+#1) write a program to find the largest number from the list
 
+"""
 num = [1,2,9,3,4,5]
 max = num[0]
 for i in num:
     if i > max:
         max = i
 print(max)
-        
+"""
+      
+#2) write a program to remove duplicates from the given list
+"""
+list1 = [1,2,2,3,4,5,5,6,5,3]
+# newlist = set(list1)              #----> set didnot allow duplicates
+# print(newlist)
+new = []
+for i in list1:
+    if i not in new:
+        new.append(i)
+print(new)
+
+"""
