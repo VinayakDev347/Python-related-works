297da12a9962d88f4843d65ac2a9183767e40a51
vinayak-dev <vinayakdev347@gmail.com>
2025-08-15 18:19:25 +0530

    commended 23Q Student Grade Management

--- problems.py
+++ problems.py
@@ -564,3 +564,30 @@ while True:
     else:
         print("invalid choice!!!")
 """
+
+#23) Student Grade Management  
+student_score = input("Enter the students score seperated with comas ")
+scores =[int(score) for score in student_score.split(",")]
+
+grades = [
+    "A" if score >= 90 else
+    "B" if score >= 80 else
+    "C" if score >= 70 else
+    "D" if score >= 60 else
+    "F"
+    for score in scores
+]
+
+passing_student = [score for score in scores if score >= 60]
+failing_student = [score for score in scores if score < 60]
+
+print("\n-----Student Grade-----\n")
+for i,(score,grade) in enumerate(zip(scores,grades),start=1):
+    print(f"Student {i}: Score = {score},Grade = {grade}")
+
+print("Student score: ",scores)
+print("Grades: ",grades)
+
+print("\n-----Passing and Failing Student-----\n")
+print("Passing Student: ",passing_student)
+print("Failing Student: ",failing_student)
