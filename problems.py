28fe9635c464b8222286432e574bdac4b35e797c
vinayak-dev <vinayakdev347@gmail.com>
2025-08-26 17:24:54 +0530

    Added 32Q Wikipedia article scrapper

--- problems.py
+++ problems.py
@@ -963,3 +963,67 @@ if event_datetime:
   print(f"Event set for: {event_datetime}")
   start_countdown(event_datetime)
 """ 
+
+#32) Wikipedia article scrapper
+
+import requests
+from bs4 import BeautifulSoup
+
+# topic ="Python_(programming_language)"
+
+def get_wikipedia_page(topic):
+    url = f"https://wikipedia.org/wiki/{topic.replace(' ','_')}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.text
+    else:
+        print(f"Failed to retrieve data. Status code: {response.status_code}. Check the topic and try again")
+        return None
+    
+def get_article_title(soup):
+    return soup.find('h1').text
+
+def get_article_summary(soup):
+    paragraphs = soup.find_all('p')
+    for para in paragraphs:
+        if para.text.strip():
+            return para.text.strip()
+    return "No Summary found"
+
+def get_headings(soup):
+    headings = [heading.text.strip() for heading in soup.find_all(['h2','h3','h4'])]
+    return headings
+
+def get_related_links(soup):
+    links = []
+    for a_tag in soup.findall('a',href=True):
+        href = a_tag['href']
+        if href.startwith('/wiki/') and ':' not in href:
+            links.append(f"https://en.wikipedia.org{href}")
+    return list(set(links))[:5]
+
+def main():
+    topic = input("Enter a topic to search on wiki pedia: ").strip()
+    page_content = get_wikipedia_page(topic)
+
+    if page_content:
+        soup = BeautifulSoup(page_content,'html.parser')
+        print(f"\nArticle Title: {get_article_title(soup)}")
+        title = get_article_title(soup)
+        summary = get_article_summary(soup)
+        headings = get_headings(soup)
+        related_links = get_related_links(soup)
+
+        print("\n-----Wikipedia Article Details-----")
+        print(f"\nTitle: {title}")
+        print(f"\nSummary: {summary}")
+        print("\nHeadings:")
+        for heading in headings[:5]:
+            print(f"- {heading}")
+
+        print("\nRelated Links: ")
+        for link in related_links:
+            print(f" - {link}")
+
+if __name__ == "__main__":
+    main()
